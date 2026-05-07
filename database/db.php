b589c0b3cad6c4747d95e6ea6efb1227e362243f
shivanisable13 <shivanisable075@gmail.com>
2026-05-08 00:29:10 +0530

    Create db.php

---
+++ database/db.php
@@ -0,0 +1,11 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+$conn = mysqli_connect("pg-db","root","root","pg_rental");
+
+
+if(!$conn){
+    die("Connection Failed: " . mysqli_connect_error());
+}
+?>
